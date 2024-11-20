cc25c3dc76578f47f9c81d26bbcdb0c2800601b1
Jerry G <jerry99george@gmail.com>
2024-11-20 04:45:07 +0000

    Added Sunset Fragment Shader

--- shaderSource.js
+++ shaderSource.js
@@ -3,29 +3,61 @@ const VertexShaderSource =
     precision mediump float;
 
     in vec4 vertPosition;
+    in vec3 vertNormal;
     out vec3 fragColour;
+    out vec3 normal;
+    out float Z;
 
     uniform mat4 u_projection;
     uniform mat4 u_camera;
-    uniform vec3 u_colour;
     
     void main()
     {
-        float prop = mod(float(gl_VertexID)/1.356267297,1.0);
         gl_Position = u_projection * u_camera  * vertPosition  ;
-        fragColour = u_colour * (1.0 - prop) + vec3(0.7,0.7,0.9) * prop;
+        normal = vertNormal;
+        Z = gl_Position.z;
     }
 `
 
-const FragmentShaderSource = 
+const NormalFragmentShaderSource = 
     `#version 300 es
     precision mediump float;
 
-    in vec3 fragColour;
+    in vec3 normal;
+    in float Z;
     out vec4 outColour;
 
     void main()
-    {
-       outColour = vec4(fragColour,1.0);
+    {   
+        vec3 normalColour = (normal+1.0)*0.5 + vec3(0.1);
+        outColour = vec4( normalColour ,1.0);
+    }
+`
+
+const SunsetFragmentShaderSource = 
+    `#version 300 es
+    precision mediump float;
+
+    in vec3 normal;
+    in float Z;
+    out vec4 outColour;
+
+    const vec3 sunset1 = vec3(160,109,186)/256.0;
+    const vec3 sunset2 = vec3(254,184,89)/256.0;
+    const vec3 sunset3 = vec3(255,228,27)/256.0;
+
+    void main()
+    {   
+        float zScaled = (Z+1.0)*0.002;
+        float ramp_blend = smoothstep(0.1,0.7,zScaled);
+
+        // fwidth(Z) gives us this fresnel-y look, helps define the corners
+        // clamping with min so its less sharp
+        float whiteblendfactor = min(fwidth(Z)*0.3,0.8)-pow(zScaled,0.7);
+    
+        vec3 blendedColour = sunset2*(1.0-ramp_blend) + sunset1*(ramp_blend);
+        vec3 finalColour = blendedColour* (1.0 - whiteblendfactor) + vec3(whiteblendfactor);
+
+        outColour = vec4( finalColour ,1.0);
     }
 `
